391d230386ae9a33151e1281cc3e8ee9b7a95d6c
lczCoder <lczcoder@163.com>
2022-12-11 23:51:31 +0800

    feat:导入chatgpt插件,完成登录和聊天发送

--- extension.js
+++ extension.js
@@ -1,24 +1,18 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
+
 function activate(context) {
   console.log("插件 【chatgpt-vscode】开始工作了!", context);
 
-	// chatgpt-explain
-	let test = vscode.commands.registerCommand(
-    "chatgpt-explain",
-    function (url) {
-			console.log('url',url)
-      vscode.window.showInformationMessage("代码解释");
-    }
-  );
+  // chatgpt-explain
+  let test = vscode.commands.registerCommand("chatgpt-explain", function (url) {
+    console.log("url", url);
+    vscode.window.showInformationMessage("代码解释");
+  });
 
   let disposable = vscode.commands.registerCommand(
     "chatgpt-vscode.helloWorld",
@@ -27,39 +21,45 @@ function activate(context) {
     }
   );
 
-  let login = vscode.commands.registerCommand(
-    "chatgpt-login",
-    async function () {
-      // vscode.window.showInformationMessage("Hello World from chatGPT-vscode!");
-      const result = vscode.window.showInputBox({
-        prompt: "在chat.openai.com/chat网页中获取cookie",
-        value: "",
-        placeHolder: "请输入身份标识",
-      });
-      result.then(inputValue => {
-        // 是按下ESC键
-        if (typeof inputValue === "undefined") return;
-        //按下enter键
-        console.log('输入内容：',inputValue);
-      });
+  let login = vscode.commands.registerCommand("chatgpt-login", userLogin);
+
+  let textCom = vscode.commands.registerTextEditorCommand(
+    "extension.selection",
+    function (textEditor) {
+      const text = textEditor.document.getText(textEditor.selection);
+      console.log("选中的文本是:", text);
     }
   );
 
-  let textCom = vscode.commands.registerTextEditorCommand('extension.selection', function(textEditor, edit) {
-		const text = textEditor.document.getText(textEditor.selection);
-		console.log('选中的文本是:', text);
-	});
-  
-
   context.subscriptions.push(disposable);
   context.subscriptions.push(test);
-	context.subscriptions.push(textCom)
-	context.subscriptions.push(login) // chatgpt 账号登录
-
+  context.subscriptions.push(textCom);
+  context.subscriptions.push(login); // chatgpt 账号登录
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
+
+async function userLogin() {
+  const { ChatGPTAPI } = await import('chatgpt')
+  // vscode.window.showInformationMessage("Hello World from chatGPT-vscode!");
+  const result = vscode.window.showInputBox({
+    prompt: "在chat.openai.com/chat网页中获取cookie",
+    value: "",
+    placeHolder: "请输入身份标识",
+  });
+  result.then(async (inputValue) => {
+    // 是按下ESC键
+    if (typeof inputValue === "undefined") return;
+    //按下enter键
+    const api = new ChatGPTAPI({
+      sessionToken: inputValue,
+    })
+    await api.ensureAuth()
+    const response = await api.sendMessage('写一个小说')
+    console.log(response)
+  });
+}
 
 module.exports = {
   activate,
